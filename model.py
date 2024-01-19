182710d016120942b7addf3d174bcca903f5324e
Trieu.Nguyen <80027464@hpctpa3pl0002.cm.cluster>
2024-01-18 23:58:02 -0500

    before fix error

--- model.py
+++ model.py
@@ -2,6 +2,8 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 class NN_Attention(nn.Module): # Neural Network Attention 
     def __init__(self, dim_model):
         super(NN_Attention, self).__init__()
@@ -64,27 +66,28 @@ class DP_Attention(nn.Module) : # Dot Product Attention
 
 
 class Encoder(nn.Module) : 
-    def  __init__(self, dim_model, num_head, num_layer, dropout, atom_dic) :
+    def  __init__(self, dim_model, num_block, num_head,  dropout, smi_dic) :
         super(Encoder, self).__init__()
-        self.Embedding = nn.Embedding(len(atom_dic), dim_model)
+        self.Embedding = nn.Embedding(len(smi_dic) - 1, dim_model)
         self.Dropout = nn.Dropout(dropout)
-        self.Encoder_Blocks = nn.ModuleList([Encoder_Block(dim_model, num_head, dropout) for _ in range(num_layer)])
+        self.Encoder_Blocks = nn.ModuleList([Encoder_Block(dim_model, num_head, dropout) for _ in range(num_block)])
 
 
     def forward(self, x) :
-        x = self.Dropout(self.Embedding(x))
 
+        x = self.Dropout(self.Embedding(x))
+        print(x.shape)
         for block in self.Encoder_Blocks : 
-            x = block(x)
+            x, last_state, self_attn= block(x)
         
-        return x 
+        return x, last_state, self_attn
     
 
 class Encoder_Block(nn.Module) :
     def __init__(self, dim_model, num_head, dropout) : 
         super(Encoder_Block, self).__init__() 
         self.Self_Attention = DP_Attention(dim_model, num_head)
-        self.LSTM = nn.LSTM(dim_model, dim_model, batch_first = True) 
+        self.GRU = nn.GRU(dim_model, dim_model, batch_first = True) 
         self.LayerNorm = nn.LayerNorm(dim_model)
 
     def forward(self, x) :  
@@ -92,7 +95,98 @@ class Encoder_Block(nn.Module) :
 
         x = self.LayerNorm(x + attn)
 
-        all_state, (h, c) = self.LSTM(x) 
+        all_state, last_state = self.GRU(x) 
 
-        return all_state, self_attn
+        return all_state, last_state, self_attn
     
+
+class DecoderBlock(nn.Module) :
+    def __init__(self, dim_model, longest_coor,dropout, num_head = 1, output_size = 3) :
+        super(DecoderBlock, self).__init__()
+
+        self.longest_coor = longest_coor
+
+        self.Cross_Attention = NN_Attention(dim_model)
+
+        self.GRU = nn.GRU(3 + dim_model, dim_model, batch_first=True)
+
+        self.Linear = nn.Linear(dim_model, output_size)
+
+        self.Dropout = nn.Dropout(dropout)
+
+    def forward(self, e_all, e_last, target = None) :
+        B = e_all.size(0)
+
+        d_input = torch.zeros(B, 1, 3).to(device)
+
+        d_hidden = e_last
+
+        d_outputs, cross_attn = [], []
+
+        for i in range(self.longest_coor) :
+            d_output, d_hidden, step_attn = self.forward_step(d_input, d_hidden, e_all)
+
+            d_outputs.append(d_output), cross_attn.append(step_attn)
+
+            if target is not None :
+                d_input = target[:, i, :].unsqueeze(1)
+            else :
+                d_input = d_output
+
+        d_outputs = torch.cat(d_outputs, dim = 1)
+
+        cross_attn = torch.cat(cross_attn, dim = 1)
+        
+        return d_outputs, d_hidden, cross_attn
+
+
+    def forward_step(self, d_input, d_hidden, e_all) :
+        Q = d_hidden.permute(1,0,2)
+
+        d_input = self.Dropout(d_input)
+
+        attn, attn_distribution = self.Cross_Attention(Q, e_all)
+
+        input_gru = torch.cat((attn, d_input), dim = 2)
+
+        output, d_hidden = self.GRU(input_gru, d_hidden) 
+        
+        output = self.Linear(output)
+
+        return output, d_hidden, attn_distribution
+    
+    
+class Decoder(nn.Module) :
+    def __init__(self, dim_model,num_block, num_head, longest_coor, dropout = 0.1) :
+        super(Decoder, self).__init__()
+        self.decoder_blocks = nn.ModuleList(
+            [DecoderBlock(dim_model, longest_coor, dropout, num_head) for _ in range(num_block)]
+        )
+    def forward(self, e_all, e_last, target = None) :
+        for block in self.decoder_blocks :
+            target, _,  cross_attn = block(e_all, e_last, target)
+        return target, cross_attn
+    
+
+class Model(nn.Module) : 
+    def __init__(self, dim_model, num_block, num_head, dropout, longest_coor, smi_dic) :
+        super(Model, self).__init__()
+
+        self.Decoder = Decoder(dim_model,
+                               num_block,
+                               num_head, 
+                               longest_coor,
+                               dropout)
+        
+        self.Encoder = Encoder(dim_model,
+                               num_block,
+                               num_head,
+                               dropout,
+                               smi_dic)
+        
+    def forward(self, x) : 
+        e_all, e_last, self_attn = self.Encoder(x) 
+
+        prediction, cross_attn = self.Decoder(e_all, e_last) 
+
+        return prediction, self_attn, cross_attn
--- utils.py
+++ utils.py
@@ -1,18 +1,18 @@
 import rdkit 
 import torch 
 import multiprocessing 
+import numpy as np
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-def get_dic(smi_list) :
-    smi_dic = {'<END>': -1, '<PAD>':0}
-    count = 1
+def get_all_atom(smi_list) :
+    atom_list = []
     for smi in smi_list :
         mol = rdkit.Chem.MolFromSmiles(smi) 
         for atom in mol.GetAtoms() :
             symbol = atom.GetSymbol() 
-            if symbol not in smi_dic :
-                smi_dic[symbol] = count 
-                count += 1 
-    return smi_dic
+            if symbol not in atom_list :
+                atom_list.append(symbol)
+    return atom_list
 
 def get_smi_list(path) :
     with open(path, 'r') as file :
@@ -36,25 +36,49 @@ def count_atom(smi) :
 
 def normalize(coor) :
     x, y, z = coor[0]
-    return torch.tensor(coor - [x,y,z])
+    return coor - [x,y,z]
 
 def pad(coor, longest_coor) :
-    zeros = torch.zeros(longest_coor - coor.size(0), 3)
-    return torch.cat((coor, zeros), dim = 0)
-
+    zeros = np.zeros((longest_coor - coor.shape[0], 3))
+    return np.concatenate((coor, zeros), axis=0)
 
 # def encode_smi(smi) :
     
+def replace_atom(smi) :
+    return smi.replace('Cl', 'X').replace('Br', 'Y').replace('Na', 'Z').replace('Ba', 'T')
+
+
+def get_dic(smi_list) :
+    smi_dic = {'<P>': 0, '<E>' : 1}
+    i = 3
+    for smi in smi_list :
+        for char in smi :
+            if char not in smi_dic :
+                smi_dic[char] = i
+                i += 1
+    return smi_dic
+
+
+def encode_smi(smi, smi_dic) :
+    smi = [smi_dic[c] for c in smi] 
+    smi = smi + [smi_dic['<E>']]
+    return smi 
+
+def pad_smi(smi, longest_smi, smi_dic) :
+    return smi + [smi_dic['<P>']] * (longest_smi - len(smi))
 
 
 
 class MyDataset(torch.utils.data.Dataset) : 
-    def __init__(self, smi_list, coor_list) : 
-        self.smi_list = smi_list
+    def __init__(self, smint_list, coor_list) : 
+        self.smint_list = smint_list
         self.coor_list = coor_list
 
     def __len__(self) :
-        return len(self.smi_list)
+        return len(self.smint_list)
     
     def __getitem__(self, idx) :
-        return self.smi_list[idx], self.coor_list[idx]
+        return torch.tensor(self.smint_list[idx],dtype = torch.long, device = device), torch.tensor(self.coor_list[idx], dtype = torch.float, device = device)  
+    
+
+
